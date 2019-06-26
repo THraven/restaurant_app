80d5b181c18ea355805f45d42edb452ccc7b6269
THraven <h_raven@live.nl>
2019-06-26 11:45:29 +0200

    added components.py to contain all the components that might be used

---
+++ components/components.py
@@ -0,0 +1,7 @@
+#!/bin/python
+"""This file contains all the components used in the app such as buttons that have to be made after loading the grid"""
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+import sys
