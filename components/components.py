47d4098e3c097157dde084879d085eb964deee14
THraven <h_raven@live.nl>
2019-06-26 13:48:44 +0200

    added category label method to components

--- components/components.py
+++ components/components.py
@@ -5,3 +5,11 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 import sys
+
+class categoryLabel(Gtk.Label):
+    """this is for the labels with the category name on them on the top of the ."""
+
+    def __init__(self, grid, text, column):
+        Gtk.Label.__init__(self, "")
+        self.set_text(text)
+        grid.attach(self, column, 0, 1, 1)
